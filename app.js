70c7c42d1f6b5836a06ad1e06aeac2a8854f1990
Tom Spitz <tjspitz1986@gmail.com>
2023-01-03 12:18:10 -0600

    Grocery List progress commit #1.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // TODO
+const GroceryList = () => {
+
+  return (
+    <div>
+      <h1>Grocery List</h1>
+        <ul>
+          <li>Bananas</li>
+          <li>Apples</li>
+        </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
